242b3ce631483856435359158b11225f96048c12
awalford16 <awalford16@gmail.com>
2023-08-16 20:15:49 +0100

    counter

--- gpio/temp_sensor/main.c
+++ gpio/temp_sensor/main.c
@@ -9,19 +9,14 @@ uint LED_SEGMENTS[SEGMENT_COUNT] = {0, 1, 2, 3, 4, 5, 6, 7};
 uint LED_DIGITS[DIGIT_COUNT] = {8, 9, 10, 11};
 
 uint DIGIT_SEGMENTS[DIGIT_COUNT][SEGMENT_COUNT] = {
-    {0, 1, 2, 3, 4, 5}, // Digit 0
-    {1, 2},             // Digit 1
-    {2, 3, 4},
+    {1, 1, 1, 1, 1, 1}, // Digit 0
+    {0, 1, 1},          // Digit 1
+    {1, 1, 0, 1, 1, 0, 1},
+    {1, 1, 1, 1, 0, 0, 1},
 };
 
 void show_digit(uint digit)
 {
-    // Disable all digits
-    for (int d = 0; d < DIGIT_COUNT; d++)
-    {
-        gpio_put(LED_DIGITS[d], 1);
-    }
-
     // Enable segments for chosen digit
     for (int s = 0; s < SEGMENT_COUNT; s++)
     {
@@ -30,6 +25,15 @@ void show_digit(uint digit)
 
     // Enable chosen digit
     gpio_put(LED_DIGITS[digit], 0);
+}
+
+void clear_digits()
+{
+    // Disable all digits
+    for (int d = 0; d < DIGIT_COUNT; d++)
+    {
+        gpio_put(LED_DIGITS[d], 1);
+    }
 }
 
 int main(void)
@@ -65,12 +69,13 @@ int main(void)
 
         printf("Temperature: %f C\n", temp);
 
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 4; i++)
         {
             show_digit(i);
-            sleep_ms(500);
+            sleep_ms(1000);
         }
 
         sleep_ms(1000);
+        clear_digits();
     }
 }
