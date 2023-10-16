b985af46f2c475f1fdefa23eb0700a5ebd7930ee
awalford16 <awalford16@gmail.com>
2023-10-16 17:10:13 +0100

    state machine

---
+++ pico/motion/README.md
@@ -0,0 +1,21 @@
+# Motion Sensor
+
+## Description
+
+Motion sensor code that works alongside an MQTT server to trigger particular events when motion is detected.
+
+## State Machine
+
+The device can be in 3 different states:
+
+### INACTIVE (0)
+
+This is when the device should not read from the motion sensor. This is useful for light triggers when the sensor does not need to be used during daytime hours
+
+### SENSING (1)
+
+This is the default state when the device is reading the motion sensor values
+
+### ON (2)
+
+This is the post-detection state after motion has been detected. It can be set to trigger another MQTT event if it has been in this state for X minutes with no motion detected.
--- pico/motion/main.py
+++ pico/motion/main.py
@@ -3,15 +3,38 @@ import machine
 import time
 from mqtt import MQTTClient
 
-mqtt_server = "192.168.0.120"  # Replace with your MQTT broker's address
-client_id = "office_motion"  # Replace with your client ID
-topic = "office/lights"  # Replace with the MQTT topic you want to publish to
+mqtt_server = "192.168.0.120"   # Replace with your MQTT broker's address
+client_id = "office_motion"     # Replace with your client ID
+topic = "office/lights"         # Replace with the MQTT topic you want to publish to
+inactive_time = 300             # Time in seconds before sending inactivity event
+
+CURRENT_STATE = 1
 
 # Set sensor pin for enabling and disabling sensor
 MOTION_SENSOR_SUPPLY_PIN = 12
 MOTION_SENSOR_VALUE_PIN = 4
 motion_sensor_out = machine.Pin(MOTION_SENSOR_SUPPLY_PIN, machine.Pin.OUT)
 motion_sensor_in = machine.Pin(MOTION_SENSOR_VALUE_PIN, machine.Pin.IN)
+
+# Callback function to disable sesor reading
+def on_message(client, userdata, message):
+    global CURRENT_STATE
+    state = message.payload.decode()
+    print(f"Setting state to {state}")
+
+    # Convert mqtt message to int
+    try:
+        enable = int(state)
+    except ValueError as verr:
+        return
+
+    # Ignore if not 0 or 1
+    if enable > 1 or enable < 0:
+        return
+    
+    # Enable/disable motion sensor by setting high/low
+    CURRENT_STATE = enable
+    motion_sensor_out.value(enable)
 
 # Create a Wi-Fi interface
 wlan = network.WLAN(network.STA_IF)
@@ -39,42 +62,36 @@ client = MQTTClient(client_id, mqtt_server)
 client.connect()
 print("Connected to MQTT broker")
 
-def connect_and_publish():
+def connect_and_publish(state):
     global client
-    client.publish(topic, "FOCUS")
-    print("PUBLISHED:", topic)
+    client.publish(topic, state)
+    print("PUBLISHED:", topic, state)
 
 # Subscribe to ENABLE topic to toggle sensor function
+client.cb = on_message
 client.subscribe("ENABLE")
-def on_message(client, userdata, message):
-    state = message.payload.decode()
-    print(f"Setting state to {state}")
 
-    # Convert mqtt message to int
-    try:
-        enable = int(state)
-    except ValueError as verr:
-        return
-
-    # Ignore if not 0 or 1
-    if enable > 1 or enable < 0:
-        return
-    
-    # Enable/disable motion sensor by setting high/low
-    motion_sensor_out.value(enable)
-
-
-last_state_on = False
+active_time = 0
 while True:
     # TODO: Perform interrupt when motion sensor is enabled/disabled
 
-    motion_detected = motion_sensor_in.value()  # Read the GPIO pin value
-    
-    if motion_detected and not last_state_on:
-        connect_and_publish()
-        last_state_on = True
-    else:
-        print("No motion.")
-        last_state_on = False
-    
-    time.sleep(5)  # Optional: Add a delay to prevent rapid reading
+    if CURRENT_STATE != 0:
+        motion_detected = motion_sensor_in.value()  # Read the GPIO pin value
+        
+        if motion_detected and CURRENT_STATE != 2:
+            connect_and_publish("FOCUS")
+            CURRENT_STATE = 2
+
+            active_time = 0
+        else:
+            print("No motion.")
+
+            if CURRENT_STATE == 2:
+                active_time += 5
+
+                # Trigger deactivate event if inactive_time is exceeded
+                if active_time > inactive_time:
+                    connect_and_publish("OFF")
+                    CURRENT_STATE = 1
+        
+        time.sleep(5)  # Optional: Add a delay to prevent rapid reading
